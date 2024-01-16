affc3ab329ceeb747a1bc23f8a0336b9f9c1ef37
Yeffian <aditchakraborty2009@gmail.com>
2024-01-16 18:08:21 +0800

    Create TabView to switch days

--- Views/ContentView.swift
+++ Views/ContentView.swift
@@ -6,12 +6,85 @@ struct ContentView: View {
         Event(eventName: "Take breakfast meds", remainder: "Take your meds after breakfast", location: "Dining Room", notes: ["Eat the meds marked 'morning'"], referenceImages: ["medicine_box", "dining_table 1"], date: CreateDate(hour: 7, minute: 40)),
         Event(eventName: "Eat lunch", remainder: "Make sure to eat lunch", location: "Dining Table", notes: ["Get a glass of water before sitting down in case you cough", "Be careful while eating fish"], referenceImages: ["dining_table 1", "dining_table 1"], date: CreateDate(hour: 14, minute: 30)),
         Event(eventName: "Take lunch meds", remainder: "Take your meds after lunch", location: "Dining Room", notes: ["Eat the meds marked 'afternoon'"], referenceImages: ["medicine_box", "dining_table 1"], date: CreateDate(hour: 14, minute: 40)),
-        
     ]
     
+    @State var day: Weekday
+    
     var body: some View {
-        NavigationView {
-            VStack {
+//        NavigationView {
+//            VStack {
+//                List {
+//                    ForEach(tasks) { task in
+//                        NavigationLink {
+//                            EventDetailView(event: task)
+//                        } label: {
+//                            VStack(alignment: .leading, spacing: 6) {
+//                                Text("\(task.eventName)")
+//                                    .font(.title2)
+//                                Text(task.date, format: .dateTime.hour().minute())
+//                                    .italic()
+//                                    .font(.footnote)
+//                            }
+//                        }
+//                    }
+//                }
+//                .listStyle(.insetGrouped)
+//                .navigationTitle("Good Morning!")
+                
+//                TabView {
+//                    Button("Sunday") {
+//                        day = Weekday.sunday
+//                    }
+//                    .tabItem {
+//                        Label("Sun", systemImage: "bolt.fill")
+//                    }
+//                    Button("Monday") {
+//                        day = Weekday.sunday
+//
+//                    }
+//                    .tabItem {
+//                        Label("Mon", systemImage: "bolt.fill")
+//                    }
+//                    Button("Tuesday") {
+//                        day = Weekday.sunday
+//                    }
+//                    .tabItem {
+//                        Label("Tues", systemImage: "bolt.fill")
+//                    }
+//                    Button("Wednesday") {
+//                        day = Weekday.sunday
+//                    }
+//                    .tabItem {
+//                        Label("Wed", systemImage: "bolt.fill")
+//                    }
+//                    Button("Thursday") {
+//                        day = Weekday.sunday
+//                    }
+//                    .tabItem {
+//                        Label("Thurs", systemImage: "bolt.fill")
+//                    }
+//                    Button("Thursday") {
+//                        day = Weekday.sunday
+//
+//                    }
+//                    .tabItem {
+//                        Label("Thurs", systemImage: "bolt.fill")
+//                    }
+//                    Button("Thursday") {
+//                        
+//                    }
+//                    .tabItem {
+//                        Label("Thurs", systemImage: "bolt.fill")
+//                    }
+//                }
+                
+                
+//            }
+            
+//            EventDetailView(event: tasks[0])
+        
+        TabView {
+            NavigationView {
                 List {
                     ForEach(tasks) { task in
                         NavigationLink {
@@ -29,41 +102,125 @@ struct ContentView: View {
                 }
                 .listStyle(.insetGrouped)
                 .navigationTitle("Good Morning!")
-                
-                TabView {
-                    Text("Sunday")
-                        .tabItem {
-                            Label("Sun", systemImage: "bolt.fill")
-                        }
-                    Text("Monday")
-                        .tabItem {
-                            Label("Mon", systemImage: "bolt.fill")
-                        }
-                    Text("Tuesday")
-                        .tabItem {
-                            Label("Tues", systemImage: "bolt.fill")
-                        }
-                    Text("Wednesday")
-                        .tabItem {
-                            Label("Wed", systemImage: "bolt.fill")
-                        }
-                    Text("Thursday")
-                        .tabItem {
-                            Label("Thurs", systemImage: "bolt.fill")
-                        }
-                    Text("Friday")
-                        .tabItem {
-                            Label("Fri", systemImage: "bolt.fill")
-                        }
-                    Text("Saturday")
-                        .tabItem {
-                            Label("Sat", systemImage: "bolt.fill")
-                        }
-                }
+            }
+            .tabItem {
+                Text("Sun")
             }
             
-            EventDetailView(event: tasks[0])
+            NavigationView {
+                List {
+                    ForEach(tasks) { task in
+                        NavigationLink {
+                            EventDetailView(event: task)
+                        } label: {
+                            VStack(alignment: .leading, spacing: 6) {
+                                Text("\(task.eventName)")
+                                    .font(.title2)
+                                Text(task.date, format: .dateTime.hour().minute())
+                                    .italic()
+                                    .font(.footnote)
+                            }
+                        }
+                    }
+                }
+                .listStyle(.insetGrouped)
+                .navigationTitle("Good Morning!")
+            }
+            .tabItem {
+                Text("Mon")
+            }
             
+            NavigationView {
+                List {
+                    ForEach(tasks) { task in
+                        NavigationLink {
+                            EventDetailView(event: task)
+                        } label: {
+                            VStack(alignment: .leading, spacing: 6) {
+                                Text("\(task.eventName)")
+                                    .font(.title2)
+                                Text(task.date, format: .dateTime.hour().minute())
+                                    .italic()
+                                    .font(.footnote)
+                            }
+                        }
+                    }
+                }
+                .listStyle(.insetGrouped)
+                .navigationTitle("Good Morning!")
+            }
+            .tabItem {
+                Text("Tues")
+            }
+            
+            NavigationView {
+                List {
+                    ForEach(tasks) { task in
+                        NavigationLink {
+                            EventDetailView(event: task)
+                        } label: {
+                            VStack(alignment: .leading, spacing: 6) {
+                                Text("\(task.eventName)")
+                                    .font(.title2)
+                                Text(task.date, format: .dateTime.hour().minute())
+                                    .italic()
+                                    .font(.footnote)
+                            }
+                        }
+                    }
+                }
+                .listStyle(.insetGrouped)
+                .navigationTitle("Good Morning!")
+            }
+            .tabItem {
+                Text("Wed")
+            }
+            
+            NavigationView {
+                List {
+                    ForEach(tasks) { task in
+                        NavigationLink {
+                            EventDetailView(event: task)
+                        } label: {
+                            VStack(alignment: .leading, spacing: 6) {
+                                Text("\(task.eventName)")
+                                    .font(.title2)
+                                Text(task.date, format: .dateTime.hour().minute())
+                                    .italic()
+                                    .font(.footnote)
+                            }
+                        }
+                    }
+                }
+                .listStyle(.insetGrouped)
+                .navigationTitle("Good Morning!")
+            }
+            .tabItem {
+                Text("Fri")
+            }
+            
+            NavigationView {
+                List {
+                    ForEach(tasks) { task in
+                        NavigationLink {
+                            EventDetailView(event: task)
+                        } label: {
+                            VStack(alignment: .leading, spacing: 6) {
+                                Text("\(task.eventName)")
+                                    .font(.title2)
+                                Text(task.date, format: .dateTime.hour().minute())
+                                    .italic()
+                                    .font(.footnote)
+                            }
+                        }
+                    }
+                }
+                .listStyle(.insetGrouped)
+                .navigationTitle("Good Morning!")
+            }
+            .tabItem {
+                Text("Sat")
+            }
         }
     }
 }
---
+++ Weekday.swift
@@ -0,0 +1,18 @@
+//
+//  Weekday.swift
+//  SSCProject
+//
+//  Created by Adit Chakraborty on 16/01/2024.
+//
+
+import Foundation
+
+enum Weekday {
+    case sunday
+    case monday
+    case tuesday
+    case wednesday
+    case thursday
+    case friday
+    case saturday
+}
